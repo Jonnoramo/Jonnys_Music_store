1179899a26fa9a2cecca3a68940f795184fc6b47
Jonnoramo <ramsden_118@hotmail.com>
2019-02-16 21:31:31 +0000

    modified accounts.views

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
@@ -7,113 +6,84 @@ from accounts.forms import UserRegistrationForm, UserLoginForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 from django.template.context_processors import csrf
+
 from django.conf import settings
 import datetime
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-from models import User
 import stripe
-import json
-from django.http import Http404, HttpResponseRedirect
+
 
 stripe.api_key = settings.STRIPE_SECRET
 
 
 def register(request):
+
     if request.method == 'POST':
-        form = UserRegistrationForm(request.POST, request.FILES)
+        form = UserRegistrationForm(request.POST)
+
         if form.is_valid():
             try:
-                customer = stripe.Customer.create(
-                    email=form.cleaned_data['email'],
+                customer = stripe.Charge.create(
+                    amount=499,
+                    currency='EUR',
+                    description=form.cleaned_data['email'],
                     card=form.cleaned_data['stripe_id'],
-                    plan='REG_MONTHLY',
                 )
             except stripe.error.CardError, e:
-                messsages.error(request, "Your card was declined")
+                messages.error(request, 'Your card was declined!')
+            if customer.paid:
+                form.save()
 
-                if user: 
-                    auth.login(request, user) 
-                    messages.success(request, "You have successfully registered") 
-                    return redirect(reverse('profile')) 
- 
- 
-                else: 
-                    messages.error(request, "We were unable to log you in at this time") 
-        else: 
-            messages.error(request, "We were unable to take payment from the card provided") 
-    else: 
-        today = datetime.date.today() 
-        form = UserRegistrationForm(initial={'expiry_month': today.month, 'expiry_year': today.year}) 
+            user = auth.authenticate(email=request.POST.get('email'),
+                                    password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered")
+                return redirect(reverse('profile'))
+
+            else:
+                messages.error(request, "Unable to log you in at this time! Please try again.")
+
+        else:
+            messages.error(request, 'We were unable to take a payment with that card. Please try another card.')
+    else:
+        today = datetime.date.today()
+        form = UserRegistrationForm()
 
     args = {'form': form, 'publishable': settings.STRIPE_PUBLISHABLE}
     args.update(csrf(request))
+
     return render(request, 'register.html', args)
 
-@login_required(login_url='/accounts/login/')
-def cancel_subscription(request):
-    try:
-        customer = stripe.Customer.retrieve(request.user.stripe_id)
-        customer.cancel_subscription(at_period_end=True)
-    except Exception, e:
-        messages.error(request, e)
-    return redirect('profile')
 
 @login_required(login_url='/login/')
 def profile(request):
     return render(request, 'profile.html')
 
-def login(request): 
-    if request.method == 'POST': 
-        form = UserLoginForm(request.POST) 
-        if form.is_valid(): 
-            user = auth.authenticate(email=request.POST.get('email'), 
-                              password=request.POST.get('password')) 
 
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(email=request.POST.get('email'),
+                                     password=request.POST.get('password'))
 
-            if user is not None: 
-                auth.login(request, user) 
-                messages.error(request, "") 
-                return redirect(reverse('profile')) 
-            else: 
-                form.add_error(None, "Your email or password was not recognised")  
-    else: 
-        form = UserLoginForm() 
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in.")
+                return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your email or password was not recognised. Please try again.")
 
+    else:
+        form = UserLoginForm()
 
     args = {'form': form}
     args.update(csrf(request))
     return render(request, 'login.html', args)
 
-@ login_required
-def get_users(request):
-    return render(request, "members.html", {'user_list': User.objects.all()})
 
 def logout(request):
     auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
-    return redirectc(reverse, 'index.html')
-    
-@csrf_exempt
-def subscriptions_webhook(request):
-    event_json = json.loads(request.body)
-
-    # Verify the event by fetching it from Stripe
-
-    try:
-        # firstly verify this is a real event generated by Stripe.com
-        # commented out for testing - uncomment when live
-        # event = stripe.Event.retrieve(event_json['object']['id'])
-
-        cust = event_json['object']['customer']
-        paid = event_json['object']['paid']
-        user = User.objects.get(stripe_id=cust)
-
-        if user and paid:
-            user.subscription_end = arrow.now().replace(weeks=+4).datetime  # add 4 weeks from now
-            user.save()
-
-    except stripe.InvalidRequestError, e:
-        return HttpResponse(status=404)
-
-    return HttpResponse(status=200)
+    messages.success(request, 'You have successfully logged out. See you soon!')
+    return render(request, 'index.html')
