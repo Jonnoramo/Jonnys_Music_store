9cecda929fee4b1f24191287144a24e84e08403c
Jonnoramo <ramsden_118@hotmail.com>
2019-02-17 07:22:21 +0000

    changed t to T in templates

--- Jonnys_Music_sotre/settings.py
+++ Jonnys_Music_sotre/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'Jonnys_Music_sotre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
