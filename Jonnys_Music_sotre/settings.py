6c6516c1c633704d19dcfe2d888dcf4ac56ff2f7
Jonnoramo <ramsden_118@hotmail.com>
2019-02-26 10:25:05 +0000

    changed background image

--- Jonnys_Music_sotre/settings.py
+++ Jonnys_Music_sotre/settings.py
@@ -88,13 +88,9 @@ WSGI_APPLICATION = 'Jonnys_Music_sotre.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
